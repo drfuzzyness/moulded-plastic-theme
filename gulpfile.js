13098e7290e87538f78733c01b581e0745ec6ac0
Matthew Conto <conto@nyu.edu>
2018-07-13 16:07:26 -0700

    Using JointsWP's script gulp ingestion now in combo with visulon's

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,8 @@ var pngquant     = require('imagemin-pngquant');
 var uglify       = require('gulp-uglify');
 var minifyCss    = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
+var filter 	     = require('gulp-filter');
+var plugin      = require('gulp-load-plugins')();
 
 // Configuration
 var config = require('./package.json').gulp;
@@ -22,67 +24,104 @@ var themePath = 'wp-content/themes/' + config.theme;
 var distPath  = themePath + '/dist';
 var distPathAbsolute = '/' + distPath;
 var assetPath = themePath + '/assets';
+const FOUNDATION = 'node_modules/foundation-sites';
+var SOURCE = {
+	scripts: [
+		// Lets grab what-input first
+	    'node_modules/what-input/dist/what-input.js',
+
+		// Foundation core - needed if you want to use any of the components below
+		FOUNDATION + '/dist/js/plugins/foundation.core.js',
+		FOUNDATION + '/dist/js/plugins/foundation.util.*.js',
+
+		// Pick the components you need in your project
+		FOUNDATION + '/dist/js/plugins/foundation.abide.js',
+		FOUNDATION + '/dist/js/plugins/foundation.accordion.js',
+		FOUNDATION + '/dist/js/plugins/foundation.accordionMenu.js',
+		FOUNDATION + '/dist/js/plugins/foundation.drilldown.js',
+		FOUNDATION + '/dist/js/plugins/foundation.dropdown.js',
+		FOUNDATION + '/dist/js/plugins/foundation.dropdownMenu.js',
+		FOUNDATION + '/dist/js/plugins/foundation.equalizer.js',
+		FOUNDATION + '/dist/js/plugins/foundation.interchange.js',
+		FOUNDATION + '/dist/js/plugins/foundation.offcanvas.js',
+		FOUNDATION + '/dist/js/plugins/foundation.orbit.js',
+		FOUNDATION + '/dist/js/plugins/foundation.responsiveAccordionTabs.js',
+		FOUNDATION + '/dist/js/plugins/foundation.responsiveMenu.js',
+		FOUNDATION + '/dist/js/plugins/foundation.responsiveToggle.js',
+		FOUNDATION + '/dist/js/plugins/foundation.reveal.js',
+		FOUNDATION + '/dist/js/plugins/foundation.slider.js',
+		FOUNDATION + '/dist/js/plugins/foundation.smoothScroll.js',
+		FOUNDATION + '/dist/js/plugins/foundation.magellan.js',
+		FOUNDATION + '/dist/js/plugins/foundation.sticky.js',
+		FOUNDATION + '/dist/js/plugins/foundation.tabs.js',
+		FOUNDATION + '/dist/js/plugins/foundation.toggler.js',
+		FOUNDATION + '/dist/js/plugins/foundation.tooltip.js',
+
+		// Place custom JS here, files will be concantonated, minified if ran with --production
+		themePath + 'assets/js/**/*.js',
+    ],
+}
 
 gulp.task(
-  'default',
-  [
-    'compile-styles',
-    'compile-scripts',
-    'compile-fonts',
-    'compile-images',
-    'compile-templates'
-  ],
-  function() {
-    gulp.watch(assetPath + '/scss/**/*.scss',  ['compile-styles']);
-    gulp.watch(assetPath + '/js/**/*.js',      ['compile-scripts']);
-    gulp.watch(assetPath + '/fonts/**/*',      ['compile-fonts']);
-    gulp.watch(assetPath + '/images/**/*',     ['compile-images']);
-    gulp.watch(assetPath + '/templates/**/*',  ['compile-templates']);
-  }
+	'default',
+	[
+		'compile-styles',
+		'compile-scripts',
+		'compile-fonts',
+		'compile-images',
+		'compile-templates'
+	],
+	function() {
+		gulp.watch(assetPath + '/scss/**/*.scss',  ['compile-styles']);
+		gulp.watch(assetPath + '/js/**/*.js',      ['compile-scripts']);
+		gulp.watch(assetPath + '/fonts/**/*',      ['compile-fonts']);
+		gulp.watch(assetPath + '/images/**/*',     ['compile-images']);
+		gulp.watch(assetPath + '/templates/**/*',  ['compile-templates']);
+	}
 );
 
 gulp.task('deploy-assets', function (callback) {
-  runSequence(
-    'clean',
-    ['compile-styles', 'compile-scripts', 'compile-images', 'compile-fonts', 'compile-templates'],
-    ['optimize-styles', 'optimize-scripts', 'optimize-images'],
-    'version-assets',
-    ['replace-versioned-assets-in-assets', 'replace-versioned-assets-in-templates'],
-    'gzip-assets',
-    'publish-to-s3',
-    callback
-  );
+	runSequence(
+		'clean',
+		['compile-styles', 'compile-scripts', 'compile-images', 'compile-fonts', 'compile-templates'],
+		['optimize-styles', 'optimize-scripts', 'optimize-images'],
+		'version-assets',
+		['replace-versioned-assets-in-assets', 'replace-versioned-assets-in-templates'],
+		'gzip-assets',
+		'publish-to-s3',
+		callback
+	);
 });
 
 gulp.task('clean', function () {
-  return gulp.src([
-    distPath,
-    themePath + '/*.php'
-  ], {read: false})
-    .pipe(clean({force: true}));
+	return gulp.src([
+		distPath,
+		themePath + '/*.php'
+	], {read: false})
+		.pipe(clean({force: true}));
 });
 
 // Styles
 // ------
 
 gulp.task('compile-styles', function () {
-  return (
-    gulp.src([
-      assetPath + '/scss/style.scss'
-    ])
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-    }))
-    .pipe(gulp.dest(distPath + '/css'))
-  );
+	return (
+		gulp.src([
+			assetPath + '/scss/style.scss'
+		])
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+				browsers: ['last 2 versions'],
+				cascade: false
+		}))
+		.pipe(gulp.dest(distPath + '/css'))
+	);
 });
 
 gulp.task('optimize-styles', function () {
-  return gulp.src(distPath + '/css/style.css')
-    .pipe(minifyCss())
-    .pipe(gulp.dest(distPath + '/css'));
+	return gulp.src(distPath + '/css/style.css')
+		.pipe(minifyCss())
+		.pipe(gulp.dest(distPath + '/css'));
 });
 
 
@@ -91,19 +130,21 @@ gulp.task('optimize-styles', function () {
 // -------
 
 gulp.task('compile-scripts', function () {
-  return (
-    gulp.src([
-      assetPath + '/js/script.js'
-    ])
-    .pipe(concat('script.js'))
-    .pipe(gulp.dest(distPath + '/js'))
-  );
+	return (
+		gulp.src( SOURCE.scripts )
+		.pipe(plugin.plumber(function(error) {
+            gutil.log(gutil.colors.red(error.message));
+            this.emit('end');
+        }))
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest(distPath + '/js'))
+	);
 });
 
 gulp.task('optimize-scripts', function () {
-  return gulp.src(distPath + '/js/script.js')
-    .pipe(uglify())
-    .pipe(gulp.dest(distPath + '/js/'));
+	return gulp.src(distPath + '/js/script.js')
+		.pipe(uglify())
+		.pipe(gulp.dest(distPath + '/js/'));
 });
 
 
@@ -112,18 +153,18 @@ gulp.task('optimize-scripts', function () {
 // -------
 
 gulp.task('compile-images', function () {
-  return gulp.src(assetPath + '/images/**/*')
-    .pipe(gulp.dest(distPath + '/images'));
+	return gulp.src(assetPath + '/images/**/*')
+		.pipe(gulp.dest(distPath + '/images'));
 });
 
 gulp.task('optimize-images', function () {
-  return gulp.src(distPath + '/images/**/*')
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{removeViewBox: false}],
-      use: [pngquant()]
-    }))
-    .pipe(gulp.dest(distPath + '/images'));
+	return gulp.src(distPath + '/images/**/*')
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		}))
+		.pipe(gulp.dest(distPath + '/images'));
 });
 
 
@@ -132,8 +173,8 @@ gulp.task('optimize-images', function () {
 // -----
 
 gulp.task('compile-fonts', function () {
-  return gulp.src(assetPath + '/fonts/**/*')
-    .pipe(gulp.dest(distPath + '/fonts'));
+	return gulp.src(assetPath + '/fonts/**/*')
+		.pipe(gulp.dest(distPath + '/fonts'));
 });
 
 
@@ -142,8 +183,8 @@ gulp.task('compile-fonts', function () {
 // ---------
 
 gulp.task('compile-templates', function () {
-  return gulp.src(assetPath + '/templates/**/*')
-    .pipe(gulp.dest(themePath));
+	return gulp.src(assetPath + '/templates/**/*')
+		.pipe(gulp.dest(themePath));
 });
 
 
@@ -151,34 +192,34 @@ gulp.task('compile-templates', function () {
 // -----------
 
 gulp.task('version-assets', function () {
-  return gulp.src(distPath + '/**/*')
-    .pipe(rev())
-    .pipe(gulp.dest(distPath))
-    .pipe(rev.manifest())
-    .pipe(gulp.dest(themePath));
+	return gulp.src(distPath + '/**/*')
+		.pipe(rev())
+		.pipe(gulp.dest(distPath))
+		.pipe(rev.manifest())
+		.pipe(gulp.dest(themePath));
 });
 
 gulp.task('replace-versioned-assets-in-assets', function () {
-  var dirReplacements = {};
-  dirReplacements[distPathAbsolute] = config.productionAssetURL;
+	var dirReplacements = {};
+	dirReplacements[distPathAbsolute] = config.productionAssetURL;
 
-  return gulp.src([
-      themePath + '/**/*.json',
-      distPath + '/**/*.css',
-      distPath + '/**/*.js'
-    ])
-    .pipe(collect({ replaceReved: true, dirReplacements: dirReplacements }))
-    .pipe(gulp.dest(distPath));
+	return gulp.src([
+			themePath + '/**/*.json',
+			distPath + '/**/*.css',
+			distPath + '/**/*.js'
+		])
+		.pipe(collect({ replaceReved: true, dirReplacements: dirReplacements }))
+		.pipe(gulp.dest(distPath));
 });
 
 gulp.task('replace-versioned-assets-in-templates', function () {
-  var dirReplacements = {};
-  dirReplacements[distPathAbsolute] = config.productionAssetURL;
+	var dirReplacements = {};
+	dirReplacements[distPathAbsolute] = config.productionAssetURL;
 
-  return gulp.src([
-      themePath + '/**/*.json',
-      themePath + '/*.php'
-    ])
-    .pipe(collect({ replaceReved: true, dirReplacements: dirReplacements }))
-    .pipe(gulp.dest(themePath));
+	return gulp.src([
+			themePath + '/**/*.json',
+			themePath + '/*.php'
+		])
+		.pipe(collect({ replaceReved: true, dirReplacements: dirReplacements }))
+		.pipe(gulp.dest(themePath));
 });
